0cee95dc37343c571e4e311eadd6c4898695a536
boop9 <85202743+boop9@users.noreply.github.com>
2026-05-09 18:20:06 +0500

    client

---
+++ src/main/java/net/boop9/horsingaround/HorsingAroundClient.java
@@ -0,0 +1,11 @@
+package net.boop9.horsingaround;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HorsingAroundClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
