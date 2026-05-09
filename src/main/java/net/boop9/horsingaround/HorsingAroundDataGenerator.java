f3aa06c2b7d4eb0445d90db267095279b1b621b7
boop9 <85202743+boop9@users.noreply.github.com>
2026-05-09 18:09:13 +0500

    Initial commit

---
+++ src/main/java/net/boop9/horsingaround/HorsingAroundDataGenerator.java
@@ -0,0 +1,11 @@
+package net.boop9.horsingaround;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HorsingAroundDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
